a554aa6dabfb6d7ecebd4b8098a20ef3fbd66b95
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-05-22 14:45:33 -0400

    Added test file and tested for full coverage.

---
+++ helpers/sql.test.js
@@ -0,0 +1,26 @@
+import { describe, test, expect } from "vitest";
+
+import { sqlForPartialUpdate } from "./sql.js";
+
+describe("test sqlForPartialUpdate", function () {
+  test("valid input", function () {
+    const result = sqlForPartialUpdate({
+      firstName: 'Aliya', age: 32
+    },
+      {
+        firstName: "first_name",
+        age: "age"
+      });
+    expect(result).toEqual(
+      {
+        "setCols": '"first_name"=$1, "age"=$2',
+        "values": ["Aliya", 32]
+      });
+  });
+
+  test("invalid input", function () {
+    expect(function () {
+      sqlForPartialUpdate({}, {});
+    }).toThrowError("No data");
+  });
+});
