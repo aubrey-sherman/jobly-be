0d95789d8bfd02d880bdefe4c11e95c33a1443c8
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-05-22 17:30:03 -0400

    Added tests for the filter function in Company class.

--- models/company.test.js
+++ models/company.test.js
@@ -95,6 +95,59 @@ describe("findAll", function () {
   });
 });
 
+/************************************** filter */
+
+describe("find with filter conditions", function () {
+
+  test("finds with one filter", async function () {
+    let companies = await Company.findFiltered({ minEmployees: 2 });
+    expect(companies).toEqual([{
+      handle: "c2",
+      name: "C2",
+      numEmployees: 2,
+      description: "Desc2",
+      logoUrl: "http://c2.img",
+    }, {
+      handle: "c3",
+      name: "C3",
+      numEmployees: 3,
+      description: "Desc3",
+      logoUrl: "http://c3.img",
+    }]);
+  });
+
+  test("handles multiple filter conditions", async function () {
+    let companies = await Company.findFiltered(
+      { nameLike: "c", minEmployees: 1 });
+    expect(companies).toEqual([{
+      handle: "c1",
+      name: "C1",
+      description: "Desc1",
+      numEmployees: 1,
+      logoUrl: "http://c1.img",
+    },
+    {
+      handle: "c2",
+      name: "C2",
+      description: "Desc2",
+      numEmployees: 2,
+      logoUrl: "http://c2.img",
+    },
+    {
+      handle: "c3",
+      name: "C3",
+      description: "Desc3",
+      numEmployees: 3,
+      logoUrl: "http://c3.img",
+    }]);
+  });
+
+  test("handles no results", async function () {
+    let companies = await Company.findFiltered({ nameLike: "Google123123123" });
+    expect(companies).toEqual([]);
+  });
+});
+
 /************************************** get */
 
 describe("get", function () {
