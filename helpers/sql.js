fa7ea28b1d185bd9f0b8003935f7a57aa97a2cfe
Zach Bunting <zvrbunting@gmail.com>
2024-05-22 11:12:34 -0700

    updated sql.js

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,6 +1,18 @@
 import { BadRequestError } from "../expressError.js";
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/** Given data to update in the database, returns data in a parameterized format
+ * for the SET clause in an SQL query
+ * Input:
+ * - dataToUpdate: object, data to be converted to a parameterized SQL query
+ * that will have placeholders for variables eg. {firstName: 'Aliya', age: 32}
+ * - jsToSql: object, key is JS variable name, value is corresponding database
+ * column eg. {numEmployees: "num_employees"}
+ *
+ * Output:
+ * Returns an object with the properties `setCols` that contains a string of the
+ * SQL columns to be set with the corresponding placeholders and `values` that
+ * contains the corresponding values for the placeholders
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
@@ -8,7 +20,7 @@ function sqlForPartialUpdate(dataToUpdate, jsToSql) {
 
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
   const cols = keys.map((colName, idx) =>
-      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+    `"${jsToSql[colName] || colName}"=$${idx + 1}`,
   );
 
   return {
